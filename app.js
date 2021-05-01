891a384e5f05e0f13cd2f40f9f61646a81c4040d
Willian Git <57255222+WrsDeveloper@users.noreply.github.com>
2021-05-01 09:10:43 -0300

    commit final

---
+++ app.js
@@ -0,0 +1,24 @@
+import * as http from "http";
+import * as fs from "fs";
+import * as pdf from "html-pdf";
+
+const server = http.createServer((req, res)=>{
+    var html = fs.readFileSync("./index.html");
+
+    const options = {
+        type: "pdf",
+        format: "A4",
+        orientation: "portrait"
+    };
+
+    pdf.create(html.toString(), options).toBuffer((err, buffer)=>{
+        if(err){
+            return res.statusCode(500).json(err);
+        }
+        res.end(buffer);
+    });
+});
+
+server.listen("3000", ()=>{
+    console.log("server iniciado ");
+});
